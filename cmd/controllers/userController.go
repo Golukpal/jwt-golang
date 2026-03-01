77c12f82c5a7bd88a6ab983ecfe1d1f3d02c6e2a
Golu Kumar Pal <golukpal@ssipmt.com>
2026-03-02 03:53:51 +0530

    added db connection

--- cmd/controllers/userController.go
+++ cmd/controllers/userController.go
@@ -1,3 +1,37 @@
 package controllers
 
-func UserController() {
+import (
+	"net/http"
+
+	"github.com/Golukpal/cmd/db"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"github.com/Golukpal/cmd/helpers"
+)
+
+var usercollection *mongo.Collection = db.OpenCollection(db.Client, "user")
+var validates = validator.New()
+
+func HashPassword()
+
+func varifyPassword()
+
+func Signup()
+
+func Login()
+
+func GetUsers()
+
+func GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		 userId := c.Param("user_id")
+
+		if err := helpers.MatchUserTypeToUid(c,userId); err!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return 
+		}
+ 
+
+	}
+}
--- cmd/db/databaseConnection.go
+++ cmd/db/databaseConnection.go
@@ -1,3 +1,38 @@
 package db
 
-func 
+import (
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func DBinstance() *mongo.Client{
+	_ , err := gotdotenv.Load(".env")
+	if err!= nil{
+		log.Fatal("errro loading .env file"))
+	}
+
+	MongoDb:= os.Getenv("MONGODB_URL")
+
+	client, err := mongo.NewClient(options.Client().applyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}	
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionname string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("golang").Collection(collectionname)
+	return collection
+} 
