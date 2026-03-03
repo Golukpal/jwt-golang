2ebc3fcd2458b56d6fc91b9e542c19d2d425b7ab
Golu Kumar Pal <golukpal@ssipmt.com>
2026-03-03 23:34:20 +0530

    added auth helper

--- cmd/controllers/userController.go
+++ cmd/controllers/userController.go
@@ -1,16 +1,20 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/Golukpal/cmd/db"
+	"github.com/Golukpal/cmd/helpers"
+	"github.com/Golukpal/cmd/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
-	"github.com/Golukpal/cmd/helpers"
 )
 
-var usercollection *mongo.Collection = db.OpenCollection(db.Client, "user")
+var userCollection *mongo.Collection = db.OpenCollection(db.Client, "user")
 var validates = validator.New()
 
 func HashPassword()
@@ -31,6 +35,17 @@ func GetUser() gin.HandlerFunc{
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return 
 		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+		defer cancel()
+		if err!= nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
  
 
 	}
