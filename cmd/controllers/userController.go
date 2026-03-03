ad781a27319783b291c429d8e85c462ffcc64362
Golu Kumar Pal <golukpal@ssipmt.com>
2026-03-03 23:58:22 +0530

    added signup

--- cmd/controllers/userController.go
+++ cmd/controllers/userController.go
@@ -2,12 +2,14 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/Golukpal/cmd/db"
 	"github.com/Golukpal/cmd/helpers"
 	"github.com/Golukpal/cmd/models"
+	"github.com/Golukpal/jwt/cmd/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -21,7 +23,40 @@ func HashPassword()
 
 func varifyPassword()
 
-func Signup()
+func Signup()gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err:= c.BindJSON(&user); err!= nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error})
+			return
+		}
+
+		validationErr:= validate.Struct(user)
+		if validationErr != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while checking for email"})
+			return 
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while checking for phone"})
+			return 
+		}
+
+		}
+}
 
 func Login()
 
